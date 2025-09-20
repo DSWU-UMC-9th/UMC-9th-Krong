0a011dcd6df82a5b44e37420f7170356765bcb67
mzxxzysy <jsyjj21@gmail.com>
2025-09-21 00:29:20 +0900

    1주차 실습 - 티켓 뷰

--- UMC_iOS_Workbook/week1/Week1_Practice/Week1_Practice/TicketView.swift
+++ UMC_iOS_Workbook/week1/Week1_Practice/Week1_Practice/TicketView.swift
@@ -7,25 +7,52 @@
 
 import SwiftUI
 
+// 1주차 실습 : TicketView
 struct TicketView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .onAppear {
-            // 폰트 체크 하기
-            UIFont.familyNames.sorted().forEach { familyName in
-                print("*** \(familyName) ***")
-                UIFont.fontNames(forFamilyName: familyName).forEach { fontName in
-                    print("\(fontName)")
-                }
-                print("---------------------")
+        ZStack{
+            Image(.background)
+            VStack{
+                Spacer().frame(height: 111)
+                mainTitleGroup
+                Spacer().frame(height: 138)
+                mainBottomGroup
             }
         }
         .padding()
+    }
+    
+    private var mainTitleGroup: some View {
+        VStack{
+            Group {
+                Text("마이펫의 이중생활2")
+                    .font(.PretendardBold30)
+                    .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
+                Text("본인 + 동반 1인")
+                    .font(.PretendardMedium16)
+                Text("30,100원")
+                    .font(.PretendardBold24)
+            }
+            .foregroundStyle(.white)
+        }
+        .frame(height: 84)
+    }
+    
+    private var mainBottomGroup: some View {
+        Button(action: {
+            print("hello")
+        }, label:{
+            VStack{
+                Image(systemName: "chevron.up")
+                    .resizable()
+                    .frame(width: 18, height: 12)
+                    .foregroundStyle(Color.white)
+                Text("예매하기")
+                    .font(.PretendardSemiBold18)
+                    .foregroundStyle(Color.white)
+            }
+            .frame(width: 63, height: 40)
+        })
     }
 }
 
