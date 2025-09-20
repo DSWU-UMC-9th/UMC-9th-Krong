f66869b2e02c40879c67a84aedd6011e82505a72
mzxxzysy <jsyjj21@gmail.com>
2025-09-21 01:07:48 +0900

    ✨ Feat: 폰트 선언형 지정

---
+++ megabox/megabox/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  megabox
+//
+//  Created by 정서영 on 9/21/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ megabox/megabox/Views/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  megabox
+//
+//  Created by 정서영 on 9/21/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
