9855b4ec0df42d9dad5fd0163e7be1d31f91536d
mzxxzysy <jsyjj21@gmail.com>
2025-09-29 00:20:41 +0900

    2주차 실습 - 대표 영화 설정

---
+++ UMC_iOS_Workbook/week2/Week2_Practice/Week2_Practice/MovieModel.swift
@@ -0,0 +1,15 @@
+//
+//  MoviewModel.swift
+//  Week2_Practice
+//
+//  Created by 정서영 on 9/28/25.
+//
+
+import SwiftUI
+
+struct MovieModel {
+    let movieImage: Image
+    let movieName: String
+    let movieLike: Int
+    let movieReserCount: Double
+}
---
+++ UMC_iOS_Workbook/week2/Week2_Practice/Week2_Practice/Week2_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week2_PracticeApp.swift
+//  Week2_Practice
+//
+//  Created by 정서영 on 9/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Week2_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
