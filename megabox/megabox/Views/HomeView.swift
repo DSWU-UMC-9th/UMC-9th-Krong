6ed0804ce8c38e394e6bac51f6fdde3fa57ba2c5
mzxxzysy <jsyjj21@gmail.com>
2025-10-05 01:47:59 +0900

    ✨ Feat: 탭바 섹션 구현 (#5)

---
+++ megabox/megabox/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  megabox
+//
+//  Created by 정서영 on 10/5/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
