b25d34a46151447d1b767631265de9bcb7c10f53
mzxxzysy <jsyjj21@gmail.com>
2025-10-11 02:45:20 +0900

    ♻️ Refactor: 디테일뷰 MVVM 적용 (#5)

--- megabox/megabox/Views/MovieDetailView.swift
+++ megabox/megabox/Views/MovieDetailView.swift
@@ -12,12 +12,19 @@ struct MovieDetailView: View {
     let movieTitle: String
     let moviePoster: Image
     @Environment(\.dismiss) var dismiss
+    private let viewModel = HomeViewModel()
+    
+    private var selectedMovie: MovieDetail? {
+        viewModel.MovieDetail.first { $0.title == movieTitle }
+    }
     
     var body: some View {
         VStack{
             TopBar
-            movieInfoGroup
-            additionalInfoGroup
+            if let detail = selectedMovie{
+                movieInfoGroup(detail)
+                additionalInfoGroup(detail)
+            }
             Spacer()
         }
         .navigationBarBackButtonHidden()
@@ -38,28 +45,22 @@ struct MovieDetailView: View {
                 Spacer()
             }
         }
-        .padding(.horizontal, 15)
+        .padding(15)
     }
     
-    private var movieInfoGroup: some View {
+    private func movieInfoGroup(_ detail: MovieDetail) -> some View {
         VStack(spacing: 10){
-            Image(.scene3)
+            
+            detail.image
             
             Text(movieTitle)
                 .font(.bold24)
             
-            Text("F1: The Movie")
+            Text(detail.englishTitle)
                 .font(.semibold14)
                 .foregroundStyle(.gray03)
             
-            Text("""
-            최고가 되지 못한 전설 VS 최고가 되고 싶은 루키
-
-            한때 주목받는 유망주였지만 끔찍한 사고로 F1에서 우승하지 못하고
-            한순간에 추락한 드라이버 ‘손 헤이스’(브래드 피트).
-            그의 오랜 동료인 ‘루벤 세르반테스’(하비에르 바르뎀)에게
-            레이싱 복귀를 제안받으며 최하위 팀인 APXGP에 합류한다.
-            """)
+            Text(detail.movieInfo)
             .font(.semibold18)
             .foregroundStyle(.gray03)
             .padding(.horizontal, 15)
@@ -67,7 +68,7 @@ struct MovieDetailView: View {
         .padding(.bottom, 35)
     }
     
-    private var additionalInfoGroup: some View {
+    private func additionalInfoGroup(_ detail: MovieDetail) ->  some View {
         VStack{
             HStack{
                 VStack{
@@ -91,8 +92,8 @@ struct MovieDetailView: View {
                     .resizable()
                     .frame(width: 100, height: 120)
                 VStack(spacing: 10){
-                    Text("12세 이상 관람가")
-                    Text("2025.06.25 개봉")
+                    Text(detail.age)
+                    Text(detail.date)
                 }
                 .font(.semibold13)
                 Spacer()
