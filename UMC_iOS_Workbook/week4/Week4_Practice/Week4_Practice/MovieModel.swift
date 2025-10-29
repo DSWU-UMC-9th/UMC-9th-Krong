d2736058868d6758f9d198e9ad5d1f9e7910f529
mzxxzysy <jsyjj21@gmail.com>
2025-10-29 13:42:45 +0900

    4주차 실습 - Model 작성

--- UMC_iOS_Workbook/week4/Week4_Practice/Week4_Practice/MovieModel.swift
+++ UMC_iOS_Workbook/week4/Week4_Practice/Week4_Practice/MovieModel.swift
@@ -5,3 +5,11 @@
 //  Created by 정서영 on 10/29/25.
 //
 
+import SwiftUI
+
+struct MovieModel: Identifiable {
+    let id: UUID = .init()
+    let movieImage: Image
+    let title: String
+    let rate: Double
+}
