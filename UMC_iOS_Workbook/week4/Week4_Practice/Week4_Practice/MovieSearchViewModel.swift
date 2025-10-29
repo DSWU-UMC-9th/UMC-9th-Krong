93086edc91e51ed47749821a048bb7fce542cf83
mzxxzysy <jsyjj21@gmail.com>
2025-10-29 13:49:44 +0900

    4주차 실습 - ViewModel 작성

--- UMC_iOS_Workbook/week4/Week4_Practice/Week4_Practice/MovieSearchViewModel.swift
+++ UMC_iOS_Workbook/week4/Week4_Practice/Week4_Practice/MovieSearchViewModel.swift
@@ -5,3 +5,24 @@
 //  Created by 정서영 on 10/29/25.
 //
 
+import SwiftUI
+import Combine
+
+final class MovieSearchViewModel: ObservableObject {
+    private let model: [MovieModel] = [
+        .init(movieImage: .init(.image1), title: "미키", rate: 9.1),
+        .init(movieImage: .init(.image2), title: "토이스토리", rate: 8.2),
+        .init(movieImage: .init(.image3), title: "브루탈리스트", rate: 8.2),
+        .init(movieImage: .init(.image4), title: "백설공주", rate: 8.2),
+        .init(movieImage: .init(.image5), title: "위플래시", rate: 8.2),
+        .init(movieImage: .init(.image6), title: "콘클라베", rate: 8.2),
+        .init(movieImage: .init(.image7), title: "더폴", rate: 8.2)
+    ]
+    
+    @Published var query: String = ""
+    @Published var results: [MovieModel] = []
+    @Published var isLoading = false
+    @Published var errorMessage: String?
+
+    private var bag = Set<AnyCancellable>()
+}
