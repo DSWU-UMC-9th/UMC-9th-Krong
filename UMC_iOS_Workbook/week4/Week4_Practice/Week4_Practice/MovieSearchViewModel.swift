90edbfc12d366ad1d74fa6d87a04877edbda9686
mzxxzysy <jsyjj21@gmail.com>
2025-10-30 16:50:21 +0900

    4주차 실습 - combine 코드 작성

--- UMC_iOS_Workbook/week4/Week4_Practice/Week4_Practice/MovieSearchViewModel.swift
+++ UMC_iOS_Workbook/week4/Week4_Practice/Week4_Practice/MovieSearchViewModel.swift
@@ -25,4 +25,51 @@ final class MovieSearchViewModel: ObservableObject {
     @Published var errorMessage: String?
 
     private var bag = Set<AnyCancellable>()
+    
+    init() {
+        $query
+            .debounce(for: .milliseconds(350), scheduler: DispatchQueue.main)
+            .removeDuplicates()
+            .handleEvents(receiveOutput: { [weak self] _ in
+                self?.errorMessage = nil
+            })
+            .flatMap { query in
+                self.search(query: query)
+            }
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] completion in
+                if case .failure(let err) = completion {
+                    self?.errorMessage = "검색 실패: \(err.localizedDescription)"
+                    self?.results = []
+                }
+            } receiveValue: { [weak self] items in
+                self?.results = items
+            }
+            .store(in: &bag)
+    }
+
+    private func search(query: String) -> AnyPublisher<[MovieModel], Error> {
+        return Future<[MovieModel], Error> { [weak self] promise in
+            let delay = Double(Int.random(in: 300...700)) / 1000.0
+            guard let self else { return }
+            
+            DispatchQueue.global().asyncAfter(deadline: .now() + delay) {
+                let filtered = self.model.filter { $0.title.lowercased().contains(query) }
+                promise(.success(filtered))
+            }
+        }
+        .handleEvents(
+            receiveSubscription: { _ in
+                DispatchQueue.main.async {
+                    self.isLoading = true
+                }
+            },
+            receiveCompletion: { _ in
+                DispatchQueue.main.async {
+                    self.isLoading = false
+                }
+            }
+        )
+        .eraseToAnyPublisher()
+    }
 }
