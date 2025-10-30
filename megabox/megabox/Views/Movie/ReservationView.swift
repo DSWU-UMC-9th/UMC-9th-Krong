b2246ea167a1b570f7a4b875ee5bbd1c70ff12fd
mzxxzysy <jsyjj21@gmail.com>
2025-10-30 21:14:44 +0900

    💄 Design: 홈뷰 패딩 추가 수정 (#5)

---
+++ megabox/megabox/Views/Movie/ReservationView.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationView.swift
+//  megabox
+//
+//  Created by 정서영 on 10/30/25.
+//
+
+import SwiftUI
+
+struct ReservationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationView()
+}
